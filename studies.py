b2d6e60feace520edc6c21f9874cc4058c2be1c3
ImFenyx <103691581+ImFenyx@users.noreply.github.com>
2024-05-12 17:34:16 -0300

    1° aula

---
+++ studies.py
@@ -0,0 +1,20 @@
+# aula 1
+
+# Comentarios
+
+# "#" Permite escrever comentário
+print(123) # Posso ter comentários na frente
+# ou em baixo (não recomentado)
+
+"""
+DocStrings geralmente
+são usados como 
+comentários multilinha
+MAS NÃO SÃO COMENTÁRIOS
+"""
+
+'''
+Pode ser usado 
+com aspas
+simples também
+'''
